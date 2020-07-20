48866fd7aa2ae23e92279a66c924c8eb48810c2b
Martin Briceno <martin.briceno@live.com>
2020-07-20 17:15:19 -0400

    Added variables in Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # js-review
 review JS
+
+## Examples of Data Types
+```javascript
+let name = "Sam";
+let age = 7;
+let samIsCool = true;
+const samFriends = ['Lulu', 'Mason', 'Gigi', 'Angel'];
+const myDog = {
+    breed: 'Yorkshire Terrier',
+    colors: ['Caramel', 'White', 'Tan'],
+    weight: '5lb',
+    placesVisited: {
+        inUsa = ['New York', 'Missouri', 'Main', 'Vermont'],
+        inCanada =['Toronto', 'Quebec']
+    }
+}
+
+```
---
+++ app.js
@@ -0,0 +1,48 @@
+let name = 'Martin Briceno';
+let age = 42;
+let isCool = true;
+const friends = ['Sam','Nea', 'Nataliia', 'Rome'];
+
+const tesla = {
+    industry: 'Electric Cars',
+    CEO: 'Elon Musk',
+    yearFound: 2003,
+    foundingMembers: ['Elon Musk', 'JB Straubel', 'Martin Eberard'],
+    stockTicker: 'TSLA',
+    vehicles: {
+        vehicleOne: 'Modes S',
+        vehicleTwo: 'Model X',
+        vehicleThree: 'Model 3'
+    },
+    print: function() {
+            console.log('Hello, this works with objects');
+            console.log("The CEO of Tesla is " +this.CEO);
+    }
+}
+
+console.log(name);
+console.log(age);
+console.log(isCool);
+console.log(friends);
+console.log(tesla);
+console.log(tesla.vehicles.vehicleThree);
+console.log(tesla['vehicles']['vehicleThree']);
+
+tesla.stockPrice = 1643.00 ;
+console.log(tesla);
+
+tesla.print();
+
+let name = "Sam";
+let age = 7;
+let samIsCool = true;
+const samFriends = ['Lulu', 'Mason', 'Gigi', 'Angel'];
+const myDog = {
+    breed: 'Yorkshire Terrier',
+    colors: ['Caramel', 'White', 'Tan'],
+    weight: '5lb',
+    placesVisited: {
+        inUsa = ['New York', 'Missouri', 'Main', 'Vermont'],
+        inCanada =['Toronto', 'Quebec']
+    }
+}
